4f0282a31b223f36cc5e46861b41b324470bd995
jamescoder92 <kusimbajames92@gmail.com>
2026-04-16 12:41:57 +0300

    assync

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your code here!
+async function getPosts() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const posts = await response.json();
+    displayPosts(posts);
+}
+
+    function displayPosts(posts) {
+        const ul = document.getElementById('post-list');
+
+        posts.forEach(post => {
+            const li = document.createElement('li');
+            
+            const h1 = document.createElement('h1');
+            h1.textContent = post.title;
+            
+            const p = document.createElement('p');
+            p.textContent = post.body;
+            
+            li.appendChild(h1);
+            li.appendChild(p);
+            ul.appendChild(li);
+        });
+}
+
+getPosts();
